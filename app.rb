54a132c23596c72d0067677e8ed8987e128d8485
bonochof <ryos427@yahoo.co.jp>
2019-06-29 20:12:18 +0900

    hello world

--- app.rb
+++ app.rb
@@ -2,39 +2,15 @@ require 'ovto'
 
 class MyApp < Ovto::App
   class State < Ovto::State
-    item :celsius, default: 0
-
-    def fahrenheit
-      (celsius * 9 / 5.0) + 32
-    end
   end
 
   class Actions < Ovto::Actions
-    def set_celsius(value:)
-      return {celsius: value}
-    end
-
-    def set_fahrenheit(value:)
-      new_celsius = (value - 32) * 5 / 9.0
-      return {celsius: new_celsius}
-    end
   end
 
   class MainComponent < Ovto::Component
-    def render
-      o 'div' do
-        o 'span', 'Celcius:'
-        o 'input', {
-          type: 'text',
-          onchange: ->(e){ actions.set_celsius(value: e.target.value.to_i) },
-          value: state.celsius
-        }
-        o 'span', 'Fahrenheit:'
-        o 'input', {
-          type: 'text',
-          onchange: ->(e){ actions.set_fahrenheit(value: e.target.value.to_i) },
-          value: state.fahrenheit
-        }
+    def render           # Don't miss the `:`. This is not a typo but
+      o 'div' do         # a "mandatory keyword argument".
+        o 'h1', "HELLO"  # All of the Ovto methods take keyword arguments.
       end
     end
   end
